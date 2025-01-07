d7b28da5a6e6cca5bee50b7eb101d146d1161e4e
DVR <dvrlabs.tv@gmail.com>
2025-01-07 14:52:50 -0500

    added wezterm config

---
+++ .config/wezterm/keys.lua
@@ -0,0 +1,98 @@
+local a = require('wezterm').action
+
+return {
+    {
+        key = 's',
+        mods = 'ALT',
+        action = a.SplitHorizontal {
+            args = {},
+        },
+    },
+    {
+        key = 'v',
+        mods = 'ALT',
+        action = a.SplitVertical {
+            args = {},
+        },
+    },
+    {
+        key = 't',
+        mods = 'ALT',
+        action = a.SpawnTab 'CurrentPaneDomain',
+    },
+    {
+        key = 'c',
+        mods = 'CTRL|ALT',
+        action = a.CloseCurrentTab { confirm = false },
+    },
+    {
+        key = 'c',
+        mods = 'ALT',
+        action = a.CloseCurrentPane { confirm = false },
+    },
+    {
+        key = 'h',
+        mods = 'ALT',
+        action = a.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'l',
+        mods = 'ALT',
+        action = a.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'k',
+        mods = 'ALT',
+        action = a.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'j',
+        mods = 'ALT',
+        action = a.ActivatePaneDirection 'Down',
+    },
+    {
+        key = 'h',
+        mods = 'CTRL|SHIFT',
+        action = a.ActivateTabRelative(-1),
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|SHIFT',
+        action = a.ActivateTabRelative(1),
+    },
+    {
+        key = 'h',
+        mods = 'CTRL|ALT',
+        action = a.AdjustPaneSize { 'Left', 5 },
+    },
+    {
+        key = 'j',
+        mods = 'CTRL|ALT',
+        action = a.AdjustPaneSize { 'Down', 5 },
+    },
+    {
+        key = 'k',
+        mods = 'CTRL|ALT',
+        action = a.AdjustPaneSize { 'Up', 5 },
+    },
+    {
+        key = 'l',
+        mods = 'CTRL|ALT',
+        action = a.AdjustPaneSize { 'Right', 5 },
+    },
+    -- {
+    --     key = 'c',
+    --     mods = 'CTRL',
+    --     action = a.CopyTo 'Clipboard',
+    -- },
+    -- {
+    --     key = 'v',
+    --     mods = 'CTRL',
+    --     action = a.PasteFrom 'Clipboard',
+    -- },
+    -- {
+    --     key = 'f',
+    --     mods = 'CMD',
+    --     action = a.ToggleFullScreen,
+    -- },
+}
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,20 @@
+return {
+    enable_wayland = false,
+    window_decorations = 'RESIZE',
+    hide_tab_bar_if_only_one_tab = true,
+    font_size = 14,
+    enable_scroll_bar = true,
+    scrollback_lines = 50000,
+    color_scheme = 'midnight-in-mojave',
+    colors = {
+        scrollbar_thumb = 'white',
+    },
+    window_padding = {
+        left = '0cell',
+        right = '0cell',
+        top = '0cell',
+        bottom = '0cell',
+    },
+    window_background_opacity = 0.98,
+    keys = require 'keys',
+}
